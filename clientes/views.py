6aef3bca6eb3fe36d7e5a4473cfbf33b45dfcbe8
CarlosAntonio76 <carlosantoniodapaixao@gmail.com>
2021-08-20 18:32:49 -0300

    Incio Projeto - Treino - Commit Primeiro

---
+++ clientes/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    # Constrói um dicionário para passar ao template seu contexto.
+    # Note que a chave 'msgnegrito' representa o {{ msgnegrito }} no template!
+    dicionario_contexto = {'msgnegrito': "Testando fonte em negrito..."}
+
+    # Retorna uma resposta renderizada para enviar ao cliente.
+    # Fazemos uso da função de atalho para facilitar tudo.
+    # Note que o primeiro parâmetro é o template que desejamos usar.
+    return render(request, 'home/index.html', dicionario_contexto)
+
+
+
+
+#def index(request):
+#    return HttpResponse('<h1>Ecommerce CAJB - 2021</h1>')
