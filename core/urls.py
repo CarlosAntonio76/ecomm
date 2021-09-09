5dfb9ab5912cd8eda32133efaae4ef0478c08d3f
CarlosAntonio76 <carlosantoniodapaixao@gmail.com>
2021-09-09 18:18:34 -0300

    Menus

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='inicial'),
+    path('sobre_nos/', views.sobre, name='sobre'),
+    path('bases/', views.bases, name='bases'),
+    path('fechar_pag/', views.fechar, name='fechar'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,18 @@ from django.http import HttpResponse
 def index(request):
     dicionario_contexto = {'msgnegrito': "Testando fonte em negrito..."}
     return render(request, 'home/index.html', dicionario_contexto)
+
+
+def sobre(request):
+    textoe = {'msgneg': "testando..."}
+    return render(request, 'home/index2.html', textoe)
+
+
+def bases(request):
+    textob = {'msgec': "testado..."}
+    return render(request, 'home/index.bkp', textob)
+
+
+def fechar(request):
+    textoc = {'msgex': "testess..."}
+    return render(request, 'home/fechar.html', textoc)
